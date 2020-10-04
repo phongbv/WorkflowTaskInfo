caf46f82d6a5cf109733413aa92aed383b2c2517
Bùi Văn Phong <phongproptit@outlook.com>
2020-10-05 06:49:10 +0700

    Update code

--- WorkflowTaskInfo/WorkflowTaskInfo/Form1.cs
+++ WorkflowTaskInfo/WorkflowTaskInfo/Form1.cs
@@ -66,9 +66,12 @@ namespace WorkflowTaskInfo
                     using (OracleConnection appContext = new OracleConnection(conn.AppConnectionString))
                     {
                         List<string> lstContent = new List<string>();
-                        List<string> assignUser = appContext.SqlQuery<string>("SELECT SU.USER_ID\r\n  FROM CAR_STAGE cs, CAR, SYS_USER SU\r\n " +
-                            "WHERE     cs.APPLICATION_ID = CAR.ID\r\n       AND CAR.APPLICATION_CODE = :CARCODE\r\n       " +
-                            "AND CAR.AMND_STATE = 'F'\r\n       AND cs.ACTION IS NULL\r\n       AND cs.ASSIGNEE_ID = SU.ID(+)",
+                        List<string> assignUser = appContext.SqlQuery<string>("SELECT SU.USER_ID\r\n  " +
+                            "FROM CAR_STAGE cs, CAR, SYS_USER SU\r\n " +
+                            "WHERE     cs.APPLICATION_ID = CAR.ID\r\n       " +
+                            "AND CAR.APPLICATION_CODE = :CARCODE\r\n       " +
+                            "AND CAR.AMND_STATE = 'F'\r\n       AND cs.ACTION IS NULL\r\n       " +
+                            "AND cs.ASSIGNEE_ID = SU.ID(+)",
                             oraCarCodePar).ToList();
                         bool flag2 = assignUser.Count((string e) => !string.IsNullOrEmpty(e)) != 0;
                         if (flag2)
@@ -84,7 +87,30 @@ namespace WorkflowTaskInfo
                         {
                             using (OracleConnection bpmContext = new OracleConnection(conn.BPMConnectionString))
                             {
-                                List<WFTASK> listTask = (from x in bpmContext.SqlQuery<WFTASK>("SELECT U.ORGANIZATIONAL_UNIT_NAME OU, W.*\r\n  FROM WFTASK W, BPM_ORGANIZATIONAL_UNIT U\r\n WHERE     (   PROTECTEDTEXTATTRIBUTE11 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE12 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE13 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE14 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE15 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE16 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE17 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE18 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE19 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE20 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE1 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE2 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE3 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE4 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE5 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE6 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE7 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE8 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE9 LIKE :CARCODE\r\n            OR PROTECTEDTEXTATTRIBUTE10 LIKE :CARCODE)\r\n       AND U.ORGANIZATIONAL_UNIT_ID = W.ORGANIZATIONALUNITID", new OracleParameter[]
+                                List<WFTASK> listTask = (from x in bpmContext.SqlQuery<WFTASK>(
+                                    "SELECT U.ORGANIZATIONAL_UNIT_NAME OU, W.*\r\n  " +
+                                    "FROM WFTASK W, BPM_ORGANIZATIONAL_UNIT U\r\n" +
+                                    " WHERE     (   PROTECTEDTEXTATTRIBUTE11 LIKE :CARCODE\r\n           " +
+                                    " OR PROTECTEDTEXTATTRIBUTE12 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE13 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE14 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE15 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE16 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE17 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE18 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE19 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE20 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE1 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE2 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE3 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE4 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE5 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE6 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE7 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE8 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE9 LIKE :CARCODE\r\n            " +
+                                    "OR PROTECTEDTEXTATTRIBUTE10 LIKE :CARCODE)\r\n       " +
+                                    "AND U.ORGANIZATIONAL_UNIT_ID = W.ORGANIZATIONALUNITID", new OracleParameter[]
                                 {
                                     oraCarCodePar
                                 }).ToList()
@@ -92,7 +118,13 @@ namespace WorkflowTaskInfo
                                                          select x).ToList<WFTASK>();
                                 foreach (WFTASK task in listTask)
                                 {
-                                    List<string> lstUser = appContext.SqlQuery<string>("SELECT SU.USER_ID\r\n  FROM SYS_ROLE  SR,\r\n       SYS_USER_ROLE      SUR,\r\n       DEPARTMENT         D,\r\n       SYS_USER           SU\r\n WHERE     UPPER (SR.EXT_REF_NO_1) = UPPER (:role)\r\n       AND SR.AMND_STATE = 'F'\r\n       AND D.DEPT_ID = :ou\r\n       AND SUR.AMND_STATE = 'F'\r\n       AND D.AMND_STATE = 'F'\r\n       AND SUR.ROLE_ID = SR.ID\r\n       AND D.ID = SUR.DEPT_ID\r\n       AND SUR.USER_ID = SU.ID\r\n       AND SU.AMND_STATE = 'F'", new OracleParameter[]
+                                    List<string> lstUser = appContext.SqlQuery<string>("SELECT SU.USER_ID\r\n  " +
+                                        "FROM SYS_ROLE  SR,\r\n       SYS_USER_ROLE      SUR,\r\n       DEPARTMENT         D,\r\n       SYS_USER           SU\r\n " +
+                                        "WHERE     UPPER (SR.EXT_REF_NO_1) = UPPER (:role)\r\n       AND SR.AMND_STATE = 'F'\r\n       " +
+                                        "AND D.DEPT_ID = :ou\r\n       AND SUR.AMND_STATE = 'F'\r\n       " +
+                                        "AND D.AMND_STATE = 'F'\r\n       AND SUR.ROLE_ID = SR.ID\r\n       " +
+                                        "AND D.ID = SUR.DEPT_ID\r\n       AND SUR.USER_ID = SU.ID\r\n      " +
+                                        " AND SU.AMND_STATE = 'F'", new OracleParameter[]
                                     {
                                         new OracleParameter("role", task.ASSIGNEESDISPLAYNAME),
                                         new OracleParameter("ou", task.OU)
